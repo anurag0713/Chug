6434ae38a49bcf57fa62e516c8d05428de51e813
Anurag Singh <csanurag713@gmail.com>
2026-02-25 21:30:31 +0530

    Initial project setup

---
+++ Chug/ChugApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChugApp.swift
+//  Chug
+//
+//  Created by Anurag Singh on 25/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ChugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
